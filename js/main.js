61971bbd428f0b06c02af7aabac8256364d37dd4
KennyTK <kennythakoala@gmail.com>
2020-05-09 22:48:53 -0400

    Fix header padding, main.js error

--- js/main.js
+++ js/main.js
@@ -20,4 +20,4 @@ function openTab(pageName, elmnt, color) {
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+//document.getElementById("defaultOpen").click();
